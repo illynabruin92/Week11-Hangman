087361b7cdb48f168b64e347ae0d1a36f8038e74
illynabruin92 <alvaivette@g.ucla.edu>
2016-09-16 19:01:54 -0700

    ideas for saving inputs

--- main.js
+++ main.js
@@ -107,3 +107,100 @@ document.onkeyup = function(event) {
 	checkLetters(letterGuessed); // runs the code to check for correctness
 	roundComplete(); // runs the code after each round is done
 }
+
+==========================================================================
+
+// Use process.argv [2] [3] to read letter input
+
+// SOLUTION 1 - More Obvious
+var a = process.argv[2];
+var b = process.argv[3];
+
+if (a == b){
+	console.log(true);
+}
+else {
+	console.log(false);
+}
+
+
+// SOLUTION 2 - Simplified (Refactored)
+console.log(process.argv[2] == process.argv[3]);
+
+
+
+
+==========================================================================
+
+//example code for saving inputs 
+
+
+// Load the NPM Package inquirer
+var inquirer = require('inquirer');
+
+// Create a "Prompt" with a series of questions.
+inquirer.prompt([
+
+	// Here we create a basic text prompt.
+	{
+		type: "input",
+		message: "What is your name?",
+		name: "name"
+	},
+
+	// Here we create a basic password-protected text prompt.
+	{
+		type: "password",
+		message: "Set your password",
+		name: "password"
+	},
+
+	// Here we give the user a list to choose from.
+	{
+		type: "checkbox",
+		message: "Which Pokemon do you choose?",
+		choices: ["Bulbasaur", "Squirtle", "Charmander"],
+		name: "pokemon"
+	},
+
+	// Here we ask the user to confirm.
+	{
+		type: "confirm",
+		message: "Are you sure:",
+		name: "confirm",
+		default: false
+
+	}
+
+// Once we are done with all the questions... "then" we do stuff with the answers
+// In this case, we store all of the answers into a "user" object that inquirer makes for us.
+]).then(function (user) {
+
+
+	// If we log that user as a JSON, we can see how it looks.
+	console.log(JSON.stringify(user, null, 2));
+
+	// If the user confirms, we displays the user's name and pokemon from the answers.
+	if (user.confirm){
+
+		console.log("==============================================");
+		console.log("");
+		console.log("Welcome " + user.name);
+		console.log("Your " + user.pokemon + " is ready for battle!");
+		console.log("");
+		console.log("==============================================");
+
+	// If the user does not confirm, then a message is provided and the program quits.
+	}
+
+	else {
+
+		console.log("");
+		console.log("");
+		console.log("That's okay " + user.name + ", come again when you are more sure.");
+		console.log("");
+		console.log("");
+
+	}
+
+});
